49a5c23af70f4b04349ddf8d54cb1e07fcf9568e
DavideBri <bbrienza99@gmail.com>
2024-11-22 00:35:05 +0100

    New settings

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NAME: "Davide Brienza",
+  EMAIL: "davidebrienza [at] proton [dot] me",
+  NUM_POSTS_ON_HOMEPAGE: 4,
+  NUM_WORKS_ON_HOMEPAGE: 1,
+  NUM_PROJECTS_ON_HOMEPAGE: 2,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "just a personal blog.",
 };
 
 export const BLOG: Metadata = {
@@ -19,26 +19,23 @@ export const BLOG: Metadata = {
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
+  TITLE: "About Me",
   DESCRIPTION: "Where I have worked and what I have done.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION:
+    "A collection of my projects, with links to repositories and demos.",
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+  {
+    NAME: "Mastodon",
+    HREF: "https://hachyderm.io/@davidebri",
   },
-  { 
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+  {
+    NAME: "Github",
+    HREF: "https://github.com/DavideBri",
   },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
 ];
